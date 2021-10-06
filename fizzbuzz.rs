de69b38532996ab6f16897df95f5ad1ef47988bf
Alex Wolf <flowalex@vivaldi.net>
2021-10-06 07:36:28 -0500

    Create fizzbuzz.rs

---
+++ fizzbuzz.rs
@@ -0,0 +1,15 @@
+// cerner_2tothe5th_2021
+
+fn main() {
+    for i in 0..=100 {
+        if i % 15 == 0{
+            println!("fizzbuzz");
+        } else if i % 3 == 0 {
+            println!("fizz");
+        } else if i % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", i);
+        }
+    }
+}
