94f4b59398633d50befee0ef553099e3b438197a
Alex Wolf <flowalex@vivaldi.net>
2021-11-01 19:09:57 -0500

    Create grep.rs

---
+++ grep.rs
@@ -0,0 +1,12 @@
+use std::env;
+// cerner_2tothe5th_2021
+// learning how to use cli flags
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+}
